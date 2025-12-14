506fab118814bcd5fa0841d034bcc98d81875183
Stephanie Apaza <stef.53apza@gmail.com>
2025-12-14 18:58:30 -0400

    Corrección en el código, usando snake_case y variables descriptivas

--- retos_sesion_06/ejercicio_01.py
+++ retos_sesion_06/ejercicio_01.py
@@ -1,61 +1,62 @@
 # Definición
 class Pasajero:
-    def __init__(self, nombre, ubicacion_destino):
+    def __init__(self, nombre, destino):
         self.nombre = nombre
-        self.ubicacion_destino = ubicacion_destino
+        self.destino = destino
         self.en_minibus = False
 
-    def subir_minibus(self, minibus):
+    def subir(self, minibus):
         """El pasajero sube al minibus"""
-        if self.ubicacion_destino in minibus.lista_paradas:
+        if self.destino in minibus.paradas_programadas:
             if not self.en_minibus:
-                minibus.subir_pasajero(self)
+                minibus.agregar_pasajero(self)
                 self.en_minibus = True
                 print(f"{self.nombre} subió al minibus {minibus.numero_ruta}")
         else:
             print(f"{self.nombre} no puede subir: su destino no está en el recorrido.")
 
-    def bajar_minibus(self, minibus):
+    def bajar(self, minibus):
         """El pasajero baja del minibus"""
-        if self.en_minibus and minibus.parada_actual == self.ubicacion_destino:
-            minibus.bajar_pasajero(self)
+        if self.en_minibus and minibus.parada_actual == self.destino:
+            minibus.retirar_pasajero(self)
             self.en_minibus = False
-            print(f"{self.nombre} bajó en {self.ubicacion_destino}")
+            print(f"{self.nombre} bajó en {self.destino}")
         else:
-            print(f"{self.nombre} no puede bajar en {minibus.parada_actual} le falta llegar a su destino.")
+            print(f"{self.nombre} aún no llegó a su destino.")
+
 class Minibus:
-    def __init__(self, numero_ruta, lista_paradas):
+    def __init__(self, numero_ruta, paradas_programadas):
         self.numero_ruta = numero_ruta
-        self.lista_paradas = lista_paradas
+        self.paradas_programadas = paradas_programadas
         self.pasajeros = []
-        self.parada_actual = lista_paradas[0]
+        self.parada_actual = paradas_programadas[0]
         self.sentido_ruta = 1
 
-    def seguir_recorrido(self):
+    def avanzar_parada(self):
         """Simula el movimiento entre paradas circulares"""
         # Indice de la parada actual
-        parada_indice_actual = self.lista_paradas.index(self.parada_actual)
+        indice_actual_parada = self.paradas_programadas.index(self.parada_actual)
 
         # Próxima parada según el sentido 1 para seguir adelante y -1 para cambiar
-        parada_indice_siguiente = parada_indice_actual + self.sentido_ruta
+        indice_siguiente_parada = indice_actual_parada + self.sentido_ruta
 
         # Cambiar de sentido si llega al final o al inicio
-        if parada_indice_siguiente >= len(self.lista_paradas):
+        if indice_siguiente_parada >= len(self.paradas_programadas):
             self.sentido_ruta *= -1
-            parada_indice_siguiente = len(self.lista_paradas) - 2
-        elif parada_indice_siguiente < 0:
+            indice_siguiente_parada = len(self.paradas_programadas) - 2
+        elif indice_siguiente_parada < 0:
             self.sentido_ruta = 1
-            parada_indice_siguiente = 1
+            indice_siguiente_parada = 1
 
-        self.parada_actual = self.lista_paradas[parada_indice_siguiente]
+        self.parada_actual = self.paradas_programadas[indice_siguiente_parada]
         print(f"Minibus {self.numero_ruta} llegó a la parada: {self.parada_actual}")
 
-    def subir_pasajero(self, pasajero):
+    def agregar_pasajero(self, pasajero):
         """Agrega un pasajero al minibus"""
         if pasajero not in self.pasajeros:
             self.pasajeros.append(pasajero)
 
-    def bajar_pasajero(self, pasajero):
+    def retirar_pasajero(self, pasajero):
         """Disminuye un pasajero del minibus"""
         if pasajero in self.pasajeros:
             self.pasajeros.remove(pasajero)
@@ -64,22 +65,22 @@ class Minibus:
 # Lista de paradas
 paradas = ["Arce", "Prado", "Perez"]
 
-minibus1 = Minibus(723, paradas)
+ruta_minibus = Minibus(723, paradas)
 
 # Pasajeros
-pasajero1 = Pasajero("Amalia", "Prado")
-pasajero2 = Pasajero("Luis", "Perez")
-pasajero3 = Pasajero("Marcela", "Arce")
+pasajero_amalia = Pasajero("Amalia", "Prado")
+pasajero_luis = Pasajero("Luis", "Perez")
+pasajero_marcela = Pasajero("Marcela", "Arce")
 print("---------------------------------------------------------")
 # Los pasajeros suben al minibus
-pasajero1.subir_minibus(minibus1)
-pasajero2.subir_minibus(minibus1)
-pasajero3.subir_minibus(minibus1)
+pasajero_amalia.subir(ruta_minibus)
+pasajero_luis.subir(ruta_minibus)
+pasajero_marcela.subir(ruta_minibus)
 
-print(f"Minibus {minibus1.numero_ruta} inicia en la parada: {minibus1.parada_actual}")
+print(f"Minibus {ruta_minibus.numero_ruta} inicia en la parada: {ruta_minibus.parada_actual}")
 # Simular el recorrido
-for recorrido in range(4):
+for numero_recorridos in range(4):
     print("---------------------------------------------------------")
-    minibus1.seguir_recorrido()
-    for pasajero in minibus1.pasajeros[:]:
-        pasajero.bajar_minibus(minibus1)
+    ruta_minibus.avanzar_parada()
+    for pasajero in ruta_minibus.pasajeros[:]:
+        pasajero.bajar(ruta_minibus)
