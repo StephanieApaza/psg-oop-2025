95c37c8c8ea9c7c1eb796272fa00dcfd5f7c764a
Stephanie Apaza <stef.53apza@gmail.com>
2025-12-14 16:08:34 -0400

    Corrección en el código, usando snake_case y variables descriptivas

--- retos_sesion_04/ejercicio_01.py
+++ retos_sesion_04/ejercicio_01.py
@@ -19,26 +19,26 @@ class Cuenta:
         print(f"Saldo actual: {self.__saldo}")
 
     # Método para realizar depósitos
-    def realizar_deposito(self, monto):
-        if monto > 0:
-            self.__saldo += monto
-            print(f"Se realizó un depósito de {monto} el saldo actual es {self.__saldo}")
+    def realizar_deposito(self, monto_depositar):
+        if monto_depositar > 0:
+            self.__saldo += monto_depositar
+            print(f"Se realizó un depósito de {monto_depositar} el saldo actual es {self.__saldo}")
         else:
             print("El monto a depositar debe ser mayor a 0.")
 
     # Método para realizar retiros    
-    def realizar_retiro(self, monto):
-        if self.__saldo > monto:
-            self.__saldo -= monto
-            print(f"Se retiró {monto} el saldo actual es {self.__saldo}")
+    def realizar_retiro(self, monto_retirar):
+        if self.__saldo >= monto_retirar:
+            self.__saldo -= monto_retirar
+            print(f"Se retiró {monto_retirar} el saldo actual es {self.__saldo}")
         else:
-            print(f"Saldo insuficiente. Intentó retirar {monto}, pero solo tiene {self.__saldo}")
+            print(f"Saldo insuficiente. Intentó retirar {monto_retirar}, pero solo tiene {self.__saldo}")
 
 # Implementando la clase
-cuenta1 = Cuenta(134500045, "Lorena Mendoza", 1500)
-cuenta1.mostrar_datos()
+cuenta_lorena = Cuenta(134500045, "Lorena Mendoza", 1500)
+cuenta_lorena.mostrar_datos()
 print("-------------------------------------------------")
-cuenta1.realizar_deposito(500)
-cuenta1.realizar_retiro(800)
+cuenta_lorena.realizar_deposito(500)
+cuenta_lorena.realizar_retiro(800)
 print("-------------------------------------------------")
-print(f"Saldo final: {cuenta1.saldo}")
+print(f"Saldo final: {cuenta_lorena.saldo}")
