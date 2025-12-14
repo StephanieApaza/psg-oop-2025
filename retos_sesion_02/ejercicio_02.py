8dd7557c373a4434f7446fdac15bc0179aa427d4
Stephanie Apaza <stef.53apza@gmail.com>
2025-12-13 21:36:16 -0400

    Corrección en el nombre de las variables, por variables descriptivas

--- retos_sesion_02/ejercicio_02.py
+++ retos_sesion_02/ejercicio_02.py
@@ -6,15 +6,15 @@ class Vino:
         self.anio_produccion = anio_produccion
 
 print("Vinos registrados:")
-Vino1 = Vino("Antinori Tignanello", "Tinto", "Sangiovenese con Cabernet", 2018)
-Vino2 = Vino("Chateau", "Tinto", "Cabernet Sauvignon", 2015)
-Vino3 = Vino("Cloudy Bay Sauvignon Blanc", "Blanco", "Sauvignon Blanc", 2021)
-Vino4 = Vino("Moet & Chandon Imperial Brut", "Espumoso", "Pinot Noir y Meunier", 2010)
+vino_uno = Vino("Antinori Tignanello", "Tinto", "Sangiovenese con Cabernet", 2018)
+vino_dos = Vino("Chateau", "Tinto", "Cabernet Sauvignon", 2015)
+vino_tres = Vino("Cloudy Bay Sauvignon Blanc", "Blanco", "Sauvignon Blanc", 2021)
+vino_cuatro = Vino("Moet & Chandon Imperial Brut", "Espumoso", "Pinot Noir y Meunier", 2010)
 
-print("Vino 1: ", Vino1.nombre, Vino1.tipo, Vino1.cepa, Vino1.anio_produccion)
-print("Vino 2: ", Vino2.nombre, Vino2.tipo, Vino2.cepa, Vino2.anio_produccion)
-print("Vino 3: ", Vino3.nombre, Vino3.tipo, Vino3.cepa, Vino3.anio_produccion)
-print("Vino 4: ", Vino4.nombre, Vino4.tipo, Vino4.cepa, Vino4.anio_produccion)
+print("Vino 1: ", vino_uno.nombre, vino_uno.tipo, vino_uno.cepa, vino_uno.anio_produccion)
+print("Vino 2: ", vino_dos.nombre, vino_dos.tipo, vino_dos.cepa, vino_dos.anio_produccion)
+print("Vino 3: ", vino_tres.nombre, vino_tres.tipo, vino_tres.cepa, vino_tres.anio_produccion)
+print("Vino 4: ", vino_cuatro.nombre, vino_cuatro.tipo, vino_cuatro.cepa, vino_cuatro.anio_produccion)
 
 class Queso:
     def __init__(self, nombre, variedad, edad, lleva_sal):
@@ -24,10 +24,10 @@ class Queso:
         self.lleva_sal = lleva_sal
 
 print("\nQuesos registrados: ")
-Queso1 = Queso("Roquefort", "Azul - de leche de vaca", 2, True)
-Queso2 = Queso("Parmigiano-Reggiano", "Duro - de leche de vaca", 1, False)
-Queso3 = Queso("Manchego", "Semicurado - de leche de oveja manchega", 3, True)
+queso_uno = Queso("Roquefort", "Azul - de leche de vaca", 2, True)
+queso_dos = Queso("Parmigiano-Reggiano", "Duro - de leche de vaca", 1, False)
+queso_tres = Queso("Manchego", "Semicurado - de leche de oveja manchega", 3, True)
 
-print("Queso 1: ", Queso1.nombre, Queso1.variedad, Queso1.edad, Queso1.lleva_sal)
-print("Queso 2: ", Queso2.nombre, Queso2.variedad, Queso2.edad, Queso2.lleva_sal)
-print("Queso 3: ", Queso3.nombre, Queso3.variedad, Queso3.edad, Queso3.lleva_sal)
+print("Queso 1: ", queso_uno.nombre, queso_uno.variedad, queso_uno.edad, queso_uno.lleva_sal)
+print("Queso 2: ", queso_dos.nombre, queso_dos.variedad, queso_dos.edad, queso_dos.lleva_sal)
+print("Queso 3: ", queso_tres.nombre, queso_tres.variedad, queso_tres.edad, queso_tres.lleva_sal)
