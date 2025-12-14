1ed040010f3c306db42eb778a458cc126c1db22f
Stephanie Apaza <stef.53apza@gmail.com>
2025-12-14 16:43:26 -0400

    Corrección en el código, usando snake_case y variables descriptivas

--- retos_sesion_04/ejercicio_02.py
+++ retos_sesion_04/ejercicio_02.py
@@ -17,34 +17,32 @@ class Celula:
         print(f"ADN: {self.__adn}, Tipo: {self.tipo_celula}, Energía: {self.__energia}")
     
     # Método para dividir la célula
-    def dividir_celula(self, division):
-        if division > 0 and division <= self.__energia:
-            self.__energia -= division
-            print(f"La celula gastó {division} de energía al dividirse. La energía actual es: {self.__energia}.")
+    def dividir_celula(self, energia_consumida):
+        if 0 < energia_consumida <= self.__energia:
+            self.__energia -= energia_consumida
+            print(f"La celula gastó {energia_consumida} de energía al dividirse. La energía actual es: {self.__energia}.")
         else:
             print(f"No hay suficiente energía para dividirse. La energía actual es: {self.__energia}")
 
     # Método para comer de la célula
-    def comer_celula(self, alimento):
-        if alimento > 0:
-            self.__energia += alimento
-            print(f"La celula consumió {alimento} de energía. La energía actual es: {self.__energia}")
+    def comer_celula(self, energia_adquirida):
+        if energia_adquirida > 0:
+            self.__energia += energia_adquirida
+            print(f"La celula consumió {energia_adquirida} de energía. La energía actual es: {self.__energia}")
         else:
             print(f"El alimento no propociona energía. La energía actual es: {self.__energia}")
 
 # Implementando la clase
-celula1 = Celula("ACTB134", "Vegetal", 10)
-celula2 = Celula("CHTS567", "Animal", 9)
+celula_vegetal = Celula("ACTB134", "Vegetal", 10)
+celula_animal = Celula("CHTS567", "Animal", 9)
 print("--------------------------------------------------------------------")
-celula1.mostrar_datos()
-celula2.mostrar_datos()
+celula_vegetal.mostrar_datos()
+celula_animal.mostrar_datos()
 print("--------------------------------------------------------------------")
-celula1.comer_celula(2)
-celula1.dividir_celula(3)
-celula1.mostrar_datos()
+celula_vegetal.comer_celula(2)
+celula_vegetal.dividir_celula(3)
+celula_vegetal.mostrar_datos()
 print("--------------------------------------------------------------------")
-celula2.comer_celula(7)
-celula2.dividir_celula(5)
-celula2.mostrar_datos()
-
-
+celula_animal.comer_celula(7)
+celula_animal.dividir_celula(5)
+celula_animal .mostrar_datos()
